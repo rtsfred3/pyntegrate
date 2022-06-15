ec4ce9384da78f4973dc6ed4d3e8987185a0bc6e
Ryan Fredrickson <rtsfred3@gmail.com>
2022-06-15 04:33:21 -0500

    Renaming step in `ci.yml` & adding macosx versions 10.12 - 10.15.

--- generate_whl.sh
+++ generate_whl.sh
@@ -39,3 +39,7 @@ python3 setup.py bdist_wheel --plat-name=manylinux2014-s390x
 # python3 setup.py bdist_wheel --plat-name=win-amd64
 
 python3 setup.py bdist_wheel --plat-name=macosx_10_11_x86_64
+python3 setup.py bdist_wheel --plat-name=macosx_10_12_x86_64
+python3 setup.py bdist_wheel --plat-name=macosx_10_13_x86_64
+python3 setup.py bdist_wheel --plat-name=macosx_10_14_x86_64
+python3 setup.py bdist_wheel --plat-name=macosx_10_15_x86_64
