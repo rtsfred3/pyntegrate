fff111b81164de4ff6cc9bfecf0ae67beb3b722c
Ryan Fredrickson <rtsfred3@gmail.com>
2022-06-15 04:09:06 -0500

    Temporarily removing generating of Windows build of `bdist_wheel`

--- generate_whl.sh
+++ generate_whl.sh
@@ -35,7 +35,7 @@ python3 setup.py bdist_wheel --plat-name=manylinux2014-ppc64
 python3 setup.py bdist_wheel --plat-name=manylinux2014-ppc64le
 python3 setup.py bdist_wheel --plat-name=manylinux2014-s390x
 
-python3 setup.py bdist_wheel --plat-name=win32
-python3 setup.py bdist_wheel --plat-name=win-amd64
+# python3 setup.py bdist_wheel --plat-name=win32
+# python3 setup.py bdist_wheel --plat-name=win-amd64
 
 python3 setup.py bdist_wheel --plat-name=macosx_10_11_x86_64
